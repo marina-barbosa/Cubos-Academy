445dc33b259ad033b92a2cd3d01f61695fd75dfc
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-07-05 01:50:29 -0300

    add calcular distancia

---
+++ 05/Calcular Distancia entre Dois Pontos.js
@@ -0,0 +1,22 @@
+
+
+// Calcular a distância entre dois pontos : (x1, y1) e (x2, y2) 
+
+let x1 = 1
+let x2 = 1
+
+let y1 = 1
+let y2 = 4
+
+distancia = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+console.log(distancia)
+
+
+// d = \sqrt{(1 - 1)^2 + (4 - 1)^2}
+// d = \sqrt{(0)^2 + (3)^2}
+// d = \sqrt{9}
+// d = 3 
+
+
+
