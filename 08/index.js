6a490142d88299d506897476138b78b094dbb3d2
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-07-06 14:54:35 -0300

    add montanha russa

--- 08/index.js
+++ 08/index.js
@@ -1,5 +1,16 @@
-const idade = 18;
+const idade = 14;
 const possuiPatologia = false;
 const altura = 180;
-const ehEstudante = false;
+const ehEstudante = true;
+
+if (idade < 12 || idade > 65 || possuiPatologia || altura < 150) {
+    console.log("acesso negado")
+} else if (ehEstudante || idade < 18) {
+    console.log("10 reais")
+} else {
+    console.log("20 reais")
+}
+
+
+
 
