d5c9d7c9f04f39632041b4562e7f53545a2a8aad
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-07-06 15:03:20 -0300

    notas

--- 09/index.js
+++ 09/index.js
@@ -1,1 +1,14 @@
-const nota = 8.5;
+const nota = 7.8;
+
+
+if (nota < 4) {
+    console.log("E")
+} else if (nota < 5.9) {
+    console.log("D")
+} else if (nota < 7.9) {
+    console.log("C")
+} else if (nota < 8.9) {
+    console.log("B")
+} else {
+    console.log("A")
+}
