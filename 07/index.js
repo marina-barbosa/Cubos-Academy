b1374ad3ea076009814c4290221feeb0ac33d16c
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-07-06 14:49:56 -0300

    add isenção de impostos

--- 07/index.js
+++ 07/index.js
@@ -1,5 +1,16 @@
 const aposentada = false;
 const portadoraDeDoenca = false;
-const totalDeRendimentos = 3000000; //emCentavos
+const totalDeRendimentos = 30_000_00; //emCentavos
 
 //seu código aqui
+
+if (aposentada || portadoraDeDoenca) {
+    console.log("isenta")
+} else if (totalDeRendimentos <= 28_559_70) {
+    console.log("vaza leao")
+} else {
+    console.log("pega leao")
+}
+
+
+
