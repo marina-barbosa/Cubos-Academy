efd61ecc77fa642074ab3701806a92ef7865c569
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-07-05 23:16:28 -0300

    add par ou impar

--- 02/index.js
+++ 02/index.js
@@ -2,3 +2,11 @@ const jogada1 = 5;
 const jogada2 = 3;
 
 //seu código aqui
+
+if ((jogada1 + jogada2) % 2 == 0) {
+    console.log("par venceu")
+} else if ((jogada1 + jogada2) % 2 != 0) {
+    console.log("impar venceu")
+} else {
+    console.log("jogada invalida")
+}
