3797e791078c7cf99efb45224e5e77ed1e5ea9a6
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-07-06 15:33:26 -0300

    add dia da semana

--- 12/index.js
+++ 12/index.js
@@ -1,1 +1,19 @@
 const diaDaSemana = 3;
+
+if (diaDaSemana <= 0 || diaDaSemana > 7) {
+    console.log("dia invalido")
+} else if (diaDaSemana === 1) {
+    console.log("segunda")
+} else if (diaDaSemana === 2) {
+    console.log("terça")
+} else if (diaDaSemana === 3) {
+    console.log("quarta")
+} else if (diaDaSemana === 4) {
+    console.log("quinta")
+} else if (diaDaSemana === 5) {
+    console.log("sexta")
+} else if (diaDaSemana === 6) {
+    console.log("sabado")
+} else {
+    console.log("domingo")
+}
