ce7aedd02b02f65dad289b3aaa7d125ea68763bb
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-07-07 03:23:20 -0300

    add filtro de numeros

--- 11/index.js
+++ 11/index.js
@@ -1,2 +1,12 @@
 const original = [5, 7, 13, 17, 26, 34, 118, 245];
+const nova = []
+
+
+for (let num of original) {
+    if (num <= 20 && num >= 10 || num > 100) {
+        nova.push(num)
+    }
+}
+
+console.log(nova)
 
