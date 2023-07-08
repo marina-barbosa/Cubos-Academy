dce591c0428ebee3cfe84a7cd51e952b9b31589b
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-07-08 18:30:13 -0300

    extrato de compra

--- 17/index.js
+++ 17/index.js
@@ -5,4 +5,18 @@ const valorDoProduto = 100000;
 const quantidadeDoParcelamento = 10;
 
 //valor pago
-const valorPago = 300;
+const valorPago = 90000;
+
+
+let parcelaFaltante = (valorDoProduto - valorPago) / 10000
+
+let valorParcela = valorDoProduto / quantidadeDoParcelamento / 100
+
+
+if (valorPago == valorDoProduto) {
+    console.log("Divida quitada")
+} else {
+    let unidade = (parcelaFaltante === 1 ? `Resta ${parcelaFaltante} parcela` : `Restam ${parcelaFaltante} parcelas`)
+
+    console.log(unidade + ` de R$ ${valorParcela}`)
+}
