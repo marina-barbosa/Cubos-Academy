6dbde334d5ebf1f6a856ef8666d3c4a8d82e5769
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-07-08 18:09:43 -0300

    categorias

--- 10/index.js
+++ 10/index.js
@@ -1,1 +1,27 @@
+//const caractere = prompt("Digite um caractere:");
 const caractere = "E";
+
+
+
+// Função para determinar a categoria do caractere
+function determinarCategoria(caractere) {
+    let lowercaseVogais = "aeiou";
+    let uppercaseVogais = "AEIOU";
+    let consoantes = "BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz";
+    let numeros = "0123456789";
+
+    if (lowercaseVogais.includes(caractere)) {
+        return "Vogal (minúscula)";
+    } else if (uppercaseVogais.includes(caractere)) {
+        return "Vogal (maiúscula)";
+    } else if (consoantes.includes(caractere)) {
+        return "Consoante";
+    } else if (numeros.includes(caractere)) {
+        return "Número";
+    } else {
+        return "Caractere inválido";
+    }
+}
+
+let categoriaEncontrada = determinarCategoria(caractere);
+console.log(`O caractere "${caractere}" é do tipo: ${categoriaEncontrada}`);
