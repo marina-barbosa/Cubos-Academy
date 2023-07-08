0ca1b76bc94818f7d1a374e9a739cfb99b1581ca
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-07-08 17:08:13 -0300

    add nome de exibição

--- 15/index.js
+++ 15/index.js
@@ -2,3 +2,12 @@ const primeiroNome = "Mario";
 const sobrenome = "";
 const apelido = "";
 
+if (apelido) {
+    console.log(apelido)
+} else if (sobrenome) {
+    console.log(primeiroNome + " " + sobrenome)
+} else if (primeiroNome) {
+    console.log(primeiroNome)
+} else {
+    console.log("é obrigatorio preencher o primeiro nome")
+}
