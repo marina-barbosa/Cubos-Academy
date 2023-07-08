eabb2414a0d54348269d762456dd7bbf172a42d1
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-07-08 17:20:09 -0300

    add desconto

--- 13/index.js
+++ 13/index.js
@@ -3,4 +3,17 @@ const tipoDePagamento = "credito";
 
 //valor da mercadoria (centavos)
 const valorDoProduto = 13000;
+let desconto
 
+if (tipoDePagamento === "credito") {
+    desconto = 5
+} else if (tipoDePagamento === "cheque") {
+    desconto = 3
+} else if (tipoDePagamento === "debito" || tipoDePagamento === "dinheiro") {
+    desconto = 10
+} else {
+    console.log("erro")
+}
+
+let valorFinal = valorDoProduto - (valorDoProduto / 100) * desconto
+console.log(valorFinal)
