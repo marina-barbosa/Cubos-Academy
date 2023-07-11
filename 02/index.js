fa73313be2aa4f0800da59d3cc40519b9a0115ed
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-07-11 16:37:30 -0300

    lista de veiculos

---
+++ 02/index.js
@@ -0,0 +1,36 @@
+const carro = [
+    {
+        marca: "Chevrolet",
+        modelo: "Onix",
+        ano: 2021,
+        cor: "preto",
+        quantidade_portas: 4,
+        automatico: true
+    },
+    {
+        marca: "Ford",
+        modelo: "Mustang",
+        ano: 2023,
+        cor: "amarelo",
+        quantidade_portas: 2,
+        automatico: true
+    },
+    {
+        marca: "Volkswagen",
+        modelo: "Gol",
+        ano: 2022,
+        cor: "vermelho",
+        quantidade_portas: 4,
+        automatico: false
+    },
+    {
+        marca: "Toyota",
+        modelo: "Corolla",
+        ano: 2023,
+        cor: "branco",
+        quantidade_portas: 4,
+        automatico: true
+    }
+];
+
+
