eb28b74e2dfe067301cf6b8f5aa181d86314c0c7
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-08-28 22:42:46 -0300

    add exercicios consultas sql

---
+++ Modulo-03/exercicios-backend-consultas-sql-master/01/query.sql
@@ -0,0 +1,37 @@
+-- 1
+-- select id, compositor from musicas
+-- 2
+-- select composicao, tempo from musicas where tempo > 60*4
+-- 3
+-- select id, compositor, composicao from musicas where id between 47 and 123
+-- 4
+-- select * from musicas where compositor is not null and tempo < 60*5 and compositor != 'Bach'  
+-- 5
+-- select composicao, tempo from musicas where compositor ilike 'mozart' or compositor ilike 'bach'
+-- 6
+-- select * from musicas order by id desc
+-- 7 
+-- select * from musicas order by tempo desc
+-- 8
+-- select * from musicas order by tempo limit 5
+-- 9
+-- select * from musicas order by tempo desc limit 10
+-- 10
+-- select * from musicas order by tempo limit 10 offset 5
+-- 11
+-- select * from musicas limit 10 offset 3*10
+-- 12
+-- select * from musicas limit 12 offset 5*12
+-- 13
+-- select distinct compositor from musicas where compositor is not null
+-- 14 
+-- select distinct compositor, composicao from musicas
+-- 15
+-- select * from musicas where compositor like 'Bra%'
+-- 16
+-- select * from musicas where ritmo like '%troppo'
+-- 17
+-- select * from musicas where composicao ilike '%quartet%'
+-- 18
+-- select * from musicas where composicao not ilike '%quintet%'
+
